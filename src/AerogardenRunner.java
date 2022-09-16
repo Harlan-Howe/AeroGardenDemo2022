a4a104e531fe2e777d751f39de848677457160e1
Harlan Howe <hhowe7@gatech.edu>
2022-09-16 11:00:06 -0500

    Made 2 constructors in Pod and wrote stub for toString.

--- src/AerogardenRunner.java
+++ src/AerogardenRunner.java
@@ -1,3 +1,9 @@
 public class AerogardenRunner
 {
+    public static void main(String[] args)
+    {
+        Pod tempPod1 = new Pod();
+        Pod tempPod2 = new Pod("Basil", 2);
+//        Pod tempPod3 = new Pod("Parsley",100,10,2);
+    }
 }
--- src/Pod.java
+++ src/Pod.java
@@ -1,3 +1,35 @@
 public class Pod
 {
+    private String plantType;
+    private int health;
+    private int height;
+    private double waterUsagePerTick;
+
+    // vanilla constructor
+    public Pod()
+    {
+        System.out.println("Making a generic plant.");
+        plantType = "generic plant";
+        health = 100;
+        height = 0;
+        waterUsagePerTick = 1;
+    }
+    // overloaded constructor
+    public Pod(String type, double usage)
+    {
+        System.out.println("Making a specific plant.");
+        plantType = type;
+        health = 100;
+        height = 0;
+        waterUsagePerTick = usage;
+    }
+
+    public String toString()
+    {
+        String result ="";
+
+
+        return result;
+    }
+
 }

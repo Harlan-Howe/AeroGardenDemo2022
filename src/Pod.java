7a69a28e2dc08edc7e4ba90abef15d91b248d646
Harlan Howe <hhowe7@gatech.edu>
2022-09-19 11:58:35 -0500

    Oops! Only committed at end of class. Added some accessors and modifiers in Pod Added grow() with random amount in Pod.

--- src/Pod.java
+++ src/Pod.java
@@ -18,10 +18,16 @@ public class Pod
     public Pod(String type, double usage)
     {
         System.out.println("Making a specific plant.");
-        plantType = type;
-        health = 100;
-        height = 0;
-        waterUsagePerTick = usage;
+        this.plantType = type;
+        this.health = 100;
+        this.height = 0;
+        this.waterUsagePerTick = usage;
+    }
+    // another overloaded constructor
+    public Pod(int hlth)
+    {
+        this("generic",1.0);
+        health = hlth;
     }
 
     public String toString()
@@ -35,4 +41,32 @@ public class Pod
         return result;
     }
 
+    public void grow()
+    {
+        height+= (int)(Math.random()*5);
+    }
+
+    // Accessor (a.k.a. "getter") for waterUsagePerTick
+    public double getWaterUsagePerTick()
+    {
+        return waterUsagePerTick;
+    }
+
+    public String getPlantType()
+    {
+        return plantType;
+    }
+
+    public int getHeight()
+    {
+        return height;
+    }
+    //Modifier (a.k.a., "setter") for health
+    public void setHealth(int h)
+    {
+        if (h>=0)
+        {
+            health = h;
+        }
+    }
 }
